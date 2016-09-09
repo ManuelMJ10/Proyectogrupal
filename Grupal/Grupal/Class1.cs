5433573efa6a70e9c1e5da1f0f8be942d97005f0
Denisse-12 <denisse.4567@outlook.com>
2016-09-08 18:48:25 -0700

    Grupal

---
+++ Grupal/Grupal/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grupal
+{
+    class Personas 
+    {
+        public int Edad { get; set; }
+        public int Nombre { get; set; }
+    }
+}
